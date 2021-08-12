0126a19e69fac01db04f1a2414296c22e1346ad3
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-08-12 08:52:55 +0300

    almost finished 2

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -4,29 +4,76 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
 import axios from 'axios';
 import { withAuth0 } from "@auth0/auth0-react";
+import newBookForm from './newBookForm';
+import Button from 'react-bootstrap/Button';
 
 class MyFavoriteBooks extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       userEmail =this.props.auth0.user.userEmail,
-      books = []
+      books =[],
+      displayAddModal: false,
     }
   }
+  handelDisplayModal = () => {
+    this.setState({ displayAddModal: true });
+  }
 
-  componentDidMount(){
+  componentDidMount() {
     this.fetchBooks()
   }
-  fetchBooks = async ()=>{
+  fetchBooks = async () => {
     try {
       const responce = await axios.get(`${process.env.REACT_APP_SERVER}/books?email=${this.state.userEmail}`)
       this.setState({
-          books: responce.data[0]?.books || []
+        books: responce.data[0]?.books || []
       })
-  } catch (err) {
+    } catch (err) {
       alert(err.message);
+    }
   }
-}
+  addNewBook = (e) => {
+    e.preventDefault();
+
+    const body = {
+      email: this.props.auth0.user.email,
+      title: e.target.bookName.value,
+      description: e.target.description.value,
+      status: e.target.status.value,
+    };
+
+    axios.post(`${process.env.REACT_APP_SERVER}/books`, body).then(axiosResponse => {
+      this.state.books.push(axiosResponse.data.books[0]);
+      this.setState({
+        books: this.state.books
+
+      })
+
+    }).catch(error => alert(error));
+    this.setState({ displayAddModal: false });
+  }
+
+
+  deleteBook = (index) => {
+    const { user } = this.props.auth0;
+
+    const Data = {
+      email: user.email,
+    }
+    axios
+      .delete(`${process.env.REACT_APP_SERVER}/books/${index}`, { params: Data })
+      .then((dataResult) => {
+        this.setState({
+          books: dataResult.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      })
+  }
+
   render() {
     return (
       <Jumbotron>
@@ -35,6 +82,13 @@ class MyFavoriteBooks extends React.Component {
           This is a collection of my favorite books
         </p>
         <div>
+        <Button variant="secondary" onClick={() => this.handelDisplayModal()}>Add new book </Button>
+        <newBookForm 
+                 show={this.state.displayAddModal}
+                 handelDisplayModal={this.handelDisplayModal}
+                 addBook={this.addBook}
+                /> 
+
           {
             this.state.books.length > 0 &&
             this.state.books.map((value, id) => {
@@ -49,19 +103,22 @@ class MyFavoriteBooks extends React.Component {
           }
         </div>
         <div>
-            {
-              this.state.books.length > 0 &&
-              this.state.books.map((value, id) => {
-                return (
-                  <div key={id}>
-                    <p>Name :{value.name}</p>
-                    <p>Description :{value.description}</p>
-                    <p>status : {value.status}</p>
-                  </div>
-                )
-              })
-            }
-          </div>
+          {
+            this.state.books.length > 0 &&
+            this.state.books.map((value, id) => {
+              return (
+                <div key={id}>
+                  <p>Name :{value.name}</p>
+                  <p>Description :{value.description}</p>
+                  <p>status : {value.status}</p>
+                </div>
+              )
+            })
+          }
+           <div key={i}>
+                    <Button variant="outline-danger" onClick={() => this.deleteBook(i)}>Delete this book</Button>
+                    </div>
+        </div>
       </Jumbotron>
     )
   }
